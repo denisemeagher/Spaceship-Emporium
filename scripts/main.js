7e71e0d9c3c98c958cacece006fcddc30d394ffe
Denise <meagher.denise@gmail.com>
2017-04-23 13:36:17 -0400

    changes to style

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,23 @@ $(document).on('click', '.ship-btn', function(e){
   e.preventDefault()
   var self = $(this),
   product = self.data("product");
-  spaceship.slideAnimation("100%", "-100%")
+  $('.ship-name').html(product.name);
+  $('.ship-mfg').html(product.manufacturer);
+  $('.ship-class').html(product.class);
+  $('.info-image img').attr("src", spaceship.cardImage(product.name));
+  if (product.price != undefined) {
+    $('.ship-price').html(product.price);
+    }
+  $(".techspecs-left, .techspecs-right").html("");
+  var i = 0;
+  for(a in product.techspecs){
+    var techSpecsClass = (i > 3) ? ".techspecs-right" : ".techspecs-left";
+    $(techSpecsClass).append(spaceship.techSpecs(a, product.techspecs))
+    i = i+1;
+  }
+  setTimeout(function(){
+    spaceship.slideAnimation("105%", "-100%")
+  })
 })
 
 $(document).on('click', '.ship-home', function(e){
@@ -32,7 +48,7 @@ var spaceship = {
   cardHtml: function(index, activeClass, product) {
     console.log(product)
     var parseProduct = JSON.stringify(product)
-    return "<div role='presentation' class='col-md-4 col-lg-3 "+activeClass+"'>\
+    return "<div role='presentation' class='col-xs-12 col-sm-6 col-md-3 "+activeClass+"'>\
         <div class='ship-card'>\
           <img class='img-responsive card-image' src='"+spaceship.cardImage(product.name)+"'>\
           <div class='overlay'>\
@@ -64,5 +80,16 @@ var spaceship = {
     $(".ship-info-container").animate({
       left: leftPosition
     }, "slow");
+  },
+  techSpecs: function(a, techspecs){
+    return '<div class="media">\
+      <div class="media-left media-middle">\
+        <i class="fa fa-industry"></i>\
+      </div>\
+      <div class="media-body media-middle">\
+          <p class="no-margin font-16">'+ a +':</p>\
+          <strong class="font-20 s-armament">'+techspecs[a]+'</strong>\
+      </div>\
+    </div>'
   }
 }
